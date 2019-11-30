e035c1c1a50c411bd44ba4adc0f43f08e2f96b3a
Aaron Stroman <aaron.l.stroman@gmail.com>
2019-11-29 21:10:07 -0600

    Add project files.

---
+++ KMLGenerator/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace KMLGenerator
+{
+    class Program
+    {
+        //const double MINLAT = -90;
+        const double MAXLAT = 90;
+        //const double MINLONG = -180;
+        const double MAXLONG = 180;
+
+        const string KMLOPENTAGS = @"<?xml version='1.0' encoding='UTF-8'?><kml xmlns='http://www.opengis.net/kml/2.2'><Document>";
+        const string KMLCLOSETAGS = @"</Document></kml>";
+
+        static void Main(string[] args)
+        {
+            int numPlacemarks = 99;
+            string filePath = "test.kml";
+
+            if (args != null && args.Length > 0)
+            {
+                filePath = args[0];
+            }
+            else
+            {
+                //do something meaningful
+            }
+
+            if (args != null && args.Length > 1)
+            {
+                Int32.TryParse(args[1], out numPlacemarks);
+            }
+
+            var rand = new Random();
+
+            using (StreamWriter outputFile = new StreamWriter(filePath))
+            {
+                outputFile.WriteLine(KMLOPENTAGS);
+
+                double randLat = 0;
+                double randLong = 0;
+
+                for (int i = 0; i < numPlacemarks; i++)
+                {
+                    randLat = rand.NextDouble() * ((rand.NextDouble() >= 0.5) ? -1 : 1) * MAXLAT;
+                    randLong = rand.NextDouble() * ((rand.NextDouble() >= 0.5) ? -1 : 1) * MAXLONG;
+
+                    outputFile.WriteLine(CreateKmlPlacemark("Point:" + i, randLat, randLong));
+                }
+
+                outputFile.WriteLine(KMLCLOSETAGS);
+            }
+        }
+
+        static string CreateKmlPlacemark(string name, double latitude, double longitude)
+        {
+            StringBuilder newPlacemark = new StringBuilder();
+
+            newPlacemark.Append(@"<Placemark>");
+            newPlacemark.Append(@"<name>" + name + @"</name>");
+            newPlacemark.Append(@"<Point>");
+            newPlacemark.Append(@"<coordinates>" + longitude.ToString() + "," + latitude.ToString() + @"</coordinates>");
+            newPlacemark.Append(@"</Point>");
+            newPlacemark.Append(@"</Placemark>");
+
+            return newPlacemark.ToString();
+        }
+
+    }
+}
---
+++ KMLGenerator/README.md
@@ -0,0 +1,27 @@
+﻿# KMLGenerator
+
+An **extremely** simple tool to generate arbitrarily large amounts of random placemark tags in a kml file.
+
+## Usage
+
+Designed to take 2 commandline arguments. First argument is file path of kml to generate, second is number of placemarks:
+
+`KMLGenerator.exe test_2.kml 30000`
+
+## Getting Started
+
+This was built to test one of the many KML/KMZ compatible apps I have written. It is being shared here because it's just interesting enough to be useful to someone else. 
+
+Tested and built in VS2019 community.
+
+### Installing
+
+Build the release file then copy (dll and exe) to wherever you want to store your executable
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Future Improvements
+
+None planed at this time, however, there very well might be other use cases to implement and a general purpose KML generator tool could prove useful in other environments.
